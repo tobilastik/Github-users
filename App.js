24605619136e023987a27c1f9f13fd3ed545f8f2
tobilastik <rajioluwatobih@gmail.com>
2019-11-30 19:37:26 +0100

    Updated

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import React, {Component} from 'react';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
 import HomeScreen from './components/HomePage/HomeScreen';
 import GithubTabNavigator from './components/GithubPage/GithubTabNavigator';
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-	apiKey: 'AIzaSyAcxhGmoUuTk6pUcQdqjWAie3bb9JnUonA',
-	authDomain: 'github-users-a3e1e.firebaseapp.com',
-	databaseURL: 'https://github-users-a3e1e.firebaseio.com/',
-	projectId: 'github-users-a3e1e',
-	storageBucket: '',
-	messagingSenderId: '136707994033',
+  apiKey: process.env.GITHUB_API_KEY,
+  authDomain: 'github-users-a3e1e.firebaseapp.com',
+  databaseURL: 'https://github-users-a3e1e.firebaseio.com/',
+  projectId: 'github-users-a3e1e',
+  storageBucket: '',
+  messagingSenderId: '136707994033',
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp (firebaseConfig);
 
-const AppSwitchNavigator = createStackNavigator({
-	Welcome: { screen: HomeScreen },
-	Github: { screen: GithubTabNavigator },
+const AppSwitchNavigator = createStackNavigator ({
+  Welcome: {screen: HomeScreen},
+  Github: {screen: GithubTabNavigator},
 });
 
 GithubTabNavigator.navigationOptions = {
-	// Hide the header from AppNavigator stack
-	header: null,
+  // Hide the header from AppNavigator stack
+  header: null,
 };
 
-const AppContainer = createAppContainer(AppSwitchNavigator);
+const AppContainer = createAppContainer (AppSwitchNavigator);
 
 export default AppContainer;
